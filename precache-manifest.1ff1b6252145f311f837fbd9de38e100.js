715fd84ca1ffbb76fde0b37d3f7694caa58da335
ZapLex <ZapLex@yandex.ru>
2023-03-29 23:35:45 +0300

    Updates

---
+++ precache-manifest.1ff1b6252145f311f837fbd9de38e100.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a138668830bd510d782f9a3b4ac25cb4",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "992b23aedc9cac90e3d8",
+    "url": "/react-gh-pages/static/css/2.1292276e.chunk.css"
+  },
+  {
+    "revision": "c3748c257d89ad8ae51a",
+    "url": "/react-gh-pages/static/css/main.1f0f7284.chunk.css"
+  },
+  {
+    "revision": "992b23aedc9cac90e3d8",
+    "url": "/react-gh-pages/static/js/2.a3249547.chunk.js"
+  },
+  {
+    "revision": "c57c7b6209bdeba794dc858c564a43af",
+    "url": "/react-gh-pages/static/js/2.a3249547.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3748c257d89ad8ae51a",
+    "url": "/react-gh-pages/static/js/main.ee14f07f.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/react-gh-pages/static/js/main.ee14f07f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb4cc03432f93ecc6bec",
+    "url": "/react-gh-pages/static/js/runtime-main.3db2534b.js"
+  },
+  {
+    "revision": "4c7a261b323c005d002689ad79927f73",
+    "url": "/react-gh-pages/static/media/arrow-up.4c7a261b.svg"
+  },
+  {
+    "revision": "ad99d9ea697a4f0cf3b9ffdd82e99458",
+    "url": "/react-gh-pages/static/media/exit-fullscreen.ad99d9ea.svg"
+  },
+  {
+    "revision": "5756d3ffe2f5b9c11b669cadd16a057c",
+    "url": "/react-gh-pages/static/media/expand-fullscreen.5756d3ff.svg"
+  },
+  {
+    "revision": "78843a21eae8a291cbe1072e6da9b62d",
+    "url": "/react-gh-pages/static/media/moon.78843a21.svg"
+  },
+  {
+    "revision": "bc8aeaa416530823e2d13e16a55f3f8d",
+    "url": "/react-gh-pages/static/media/sun.bc8aeaa4.svg"
+  }
+]);
